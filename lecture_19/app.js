992b4943f32a106c58a72ceb677690151a8a3a7c
skybox <skybox@skyboxs-MacBook-Air.local>
2024-09-18 10:53:27 +0700

    Lecture 19 : prototype

--- lecture_19/app.js
+++ lecture_19/app.js
@@ -15,31 +15,34 @@
 
 	ChildController1.$inject = ['$scope']
 	function ChildController1($scope) {
-		console.log("$scope.parentValue: ", $scope.parentValue)
-		console.log("CHILD $scope: ", $scope)
 
-		$scope.parentValue = 5
-		console.log("*** CHANGED: $scope.parentValue = 5 ***")
-		console.log("$scope.parentValue: ",$scope.parentValue)
-		console.log($scope)
+		// console.log("$scope.parentValue: ", $scope.parentValue)
+		// console.log("CHILD $scope: ", $scope)
 
-		console.log("$scope.pc.parentValue",$scope.pc.parentValue)
-		$scope.pc.parentValue = 5
-		console.log("** CHANGED: $scope.pc.parentValue = 5; ***")
-		console.log("$scope.pc.parentValue: ",$scope.pc.parentValue)
-		console.log("$scope: ",$scope)
+		// $scope.parentValue = 5
+		// console.log("*** CHANGED: $scope.parentValue = 5 ***")
+		// console.log("$scope.parentValue: ",$scope.parentValue)
+		// console.log($scope)
 
-		console.log("$scope.$parent.parentValue: ",$scope.$parent.parentValue)
+		// console.log("$scope.pc.parentValue",$scope.pc.parentValue)
+		// $scope.pc.parentValue = 5
+		// console.log("** CHANGED: $scope.pc.parentValue = 5; ***")
+		// console.log("$scope.pc.parentValue: ",$scope.pc.parentValue)
+		// console.log("$scope: ",$scope)
+
+		// console.log("$scope.$parent.parentValue: ",$scope.$parent.parentValue)
 	}
 
-	ParentController2.$inject = ['$scope']
-	function ParentController2($scope) {
+	// ParentController2.$inject = ['$scope']
+	function ParentController2() {
 		var parent = this
 		parent.value = 1
 	}
 	ChildController2.$inject = ['$scope']
 	function ChildController2($scope) {
 		var child = this
-		child.value = 1	}
+		child.value = 5	
+		console.log("ChildController2 $scope : ",$scope)
+	}
 
 })();
