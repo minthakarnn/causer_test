d1dbcf86230b6896f1a702ae1e6ed692632ba58a
skybox <skybox@skyboxs-MacBook-Air.local>
2024-09-18 10:47:39 +0700

    Lecture 19 : prototype

--- lecture_19/app.js
+++ lecture_19/app.js
@@ -1,10 +1,10 @@
 (function () {
 	'use strict'
-	angular.module('ControllerAsApp',[])
-	.controller('ParentController1',ParentController1)
-	.controller('ChildController1',ChildController1)
-	.controller('ParentController2',ParentController2)
-	.controller('ChildController2',ChildController2)
+	angular.module('ControllerAsApp', [])
+	.controller('ParentController1', ParentController1)
+	.controller('ChildController1', ChildController1)
+	.controller('ParentController2', ParentController2)
+	.controller('ChildController2', ChildController2)
 
 	ParentController1.$inject = ['$scope']
 	function ParentController1($scope) {
@@ -12,9 +12,34 @@
 		$scope.pc = this
 		$scope.pc.parentValue = 1
 	}
+
 	ChildController1.$inject = ['$scope']
 	function ChildController1($scope) {
+		console.log("$scope.parentValue: ", $scope.parentValue)
+		console.log("CHILD $scope: ", $scope)
+
+		$scope.parentValue = 5
+		console.log("*** CHANGED: $scope.parentValue = 5 ***")
 		console.log("$scope.parentValue: ",$scope.parentValue)
-		console.log("CHILD $scope: ",$scope)
+		console.log($scope)
+
+		console.log("$scope.pc.parentValue",$scope.pc.parentValue)
+		$scope.pc.parentValue = 5
+		console.log("** CHANGED: $scope.pc.parentValue = 5; ***")
+		console.log("$scope.pc.parentValue: ",$scope.pc.parentValue)
+		console.log("$scope: ",$scope)
+
+		console.log("$scope.$parent.parentValue: ",$scope.$parent.parentValue)
 	}
-})()
+
+	ParentController2.$inject = ['$scope']
+	function ParentController2($scope) {
+		var parent = this
+		parent.value = 1
+	}
+	ChildController2.$inject = ['$scope']
+	function ChildController2($scope) {
+		var child = this
+		child.value = 1	}
+
+})();
